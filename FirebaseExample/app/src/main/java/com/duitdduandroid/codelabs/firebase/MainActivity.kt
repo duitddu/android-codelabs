f39d74fc5da09cfd69b5e3e607c6489881dab9ff
duitddu <du.it.ddu@gmail.com>
2020-12-06 18:47:33 +0900

    파이어베이스 앱 추가

---
+++ FirebaseExample/app/src/main/java/com/duitdduandroid/codelabs/firebase/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.duitdduandroid.codelabs.firebase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
