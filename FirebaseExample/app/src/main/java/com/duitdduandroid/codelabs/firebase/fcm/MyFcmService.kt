1959aa2d83d05b8ecae29295766823d6a00df5e0
duitddu <du.it.ddu@gmail.com>
2020-12-19 16:14:04 +0900

    FCM 추가

---
+++ FirebaseExample/app/src/main/java/com/duitdduandroid/codelabs/firebase/fcm/MyFcmService.kt
@@ -0,0 +1,61 @@
+package com.duitdduandroid.codelabs.firebase.fcm
+
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Intent
+import android.os.Build
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import com.duitdduandroid.codelabs.firebase.MainActivity
+import com.duitdduandroid.codelabs.firebase.R
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import java.util.*
+
+class MyFcmService : FirebaseMessagingService() {
+    override fun onNewToken(token: String) {
+        Log.d("MyFcmService", "New token :: $token")
+        sendTokenToServer(token)
+    }
+
+    private fun sendTokenToServer(token: String) {
+        // TODO :: TOKEN 값을 서버에 저장하자!
+    }
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        // TODO :: 전달받은 리모트 메시지를 처리하자!
+
+        Log.d("MyFcmService", "Notification Title :: ${remoteMessage.notification?.title}")
+        Log.d("MyFcmService", "Notification Body :: ${remoteMessage.notification?.body}")
+        Log.d("MyFcmService", "Notification Data :: ${remoteMessage.data}")
+
+        remoteMessage.notification?.let {
+            showNotification(it)
+        }
+    }
+
+    private fun showNotification(notification: RemoteMessage.Notification) {
+        val intent = Intent(this, MainActivity::class.java)
+        val pIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+        val channelId = getString(R.string.my_notification_channel_id)
+
+        val notificationBuilder = NotificationCompat.Builder(this, channelId)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setContentTitle(notification.title)
+            .setContentText(notification.body)
+            .setContentIntent(pIntent)
+
+        getSystemService(NotificationManager::class.java).run {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                val channel = NotificationChannel(channelId, "알림", NotificationManager.IMPORTANCE_HIGH)
+                createNotificationChannel(channel)
+            }
+
+            notify(Date().time.toInt(), notificationBuilder.build())
+        }
+    }
+}
